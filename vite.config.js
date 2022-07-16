6448311feb4e67f4d984f71227f2a79cfa1459d4
lucastseun <lucastseun@qq.com>
2022-07-16 22:24:51 +0800

    修改

--- vite.config.js
+++ vite.config.js
@@ -12,10 +12,10 @@ const getVer = () => {
 }
 
 // https://vitejs.dev/config/
-export default defineConfig((command, mode) => {
+export default defineConfig(({command, mode}) => {
   // 根据当前工作目录中的 `mode` 加载 .env 文件
   // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd())
   env.VITE_APP_VER = getVer()
   return {
     plugins: [vue()]
