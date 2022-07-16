da508c3acaadfe7f43473da166e09b58d6376bef
lucastseun <lucastseun@qq.com>
2022-07-16 12:53:08 +0800

    增加自定义环境变量，获取分支版本号

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { execSync } from "child_process";
+
+// 从git分支获取版本号
+const getVer = () => {
+    const branch =  execSync('git name-rev --name-only HEAD', {encoding: 'utf-8'});
+    
+    const ver = branch.split('/')[1].slice(0, 8);
+    
+    return ver;
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig((command, mode) => {
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
+  const env = loadEnv(mode, process.cwd(), '')
+  env.VITE_APP_VER = getVer()
+  return {
+    plugins: [vue()]
+  }
 })
